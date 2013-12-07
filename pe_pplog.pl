ec14fb7c49fea44c023eeb1ee9bb51c9dc886723
efiabruni <svala.3@gmail.com>
2013-12-07 15:51:42 -0300

    Added check for double posts of comments

--- pe_pplog.pl
+++ pe_pplog.pl
@@ -576,6 +576,16 @@ elsif(r('viewDetailed') ne '' || r('sendComment') ne '')
 	}
 	# End of author checking, start adding comment
 	
+	#check if the comment already exists
+	open (FILE, "<$config_commentsDatabaseFolder/$fileName.$config_dbFilesExtension");
+	while (<FILE>){
+		my $check=$title.'"'.$author.'"'.$content;
+		if ($_=~ /$check/){
+			print '<br />'.$locale{$lang}->{comtwice};
+			$do=0;
+		}
+	}
+	close FILE;
 	if($do == 1)
 	{	
 
