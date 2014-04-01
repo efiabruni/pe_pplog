5673b6b49de54adf6dec226aaa3b2489cbb39307
efiabruni <svala.3@gmail.com>
2014-03-31 21:44:56 -0300

    added a missing closing tag and added a '0 emails' message

--- blog/emails.pl
+++ blog/emails.pl
@@ -1,13 +1,13 @@
 my @emails=getFiles($config_DatabaseFolder.'/emails');
 print "<h1>$locale{$lang}->{emails}</h1>";
+
 foreach (@emails){
 
 	my @entries=split(/¬/, $_);
-	my @categories = split (/'/, $entries[3]);
-	print '<div class="article"><h1>'.$entries[0].' &nbsp; <small>
-	<a href="mailto:'.$categories[1].'">'.$categories[0].'</a> - <a href="?delete=emails/'.$entries[4].'">'.$locale{$lang}->{d}.'</a></small></h1>
-	<div class="hide"><br /><br />'.$entries[1].'<br /></div></br>
-	<footer>'.$locale{$lang}->{postedon}.$entries[2].'</footer>';
+	my @address = split (/'/, $entries[3]);
+	print '<h1>'.$entries[0].' &nbsp; <small>
+	<a href="mailto:'.$address[1].'">'.$address[0].'</a> - <a href="?delete=emails/'.$entries[4].'">'.$locale{$lang}->{d}.'</a></small></h1>
+	<div class="full">'.$entries[1].'<br /></br>
+	<footer>'.$locale{$lang}->{postedon}.$entries[2].'</footer></div>';
 }
-print'';	
-
+print "0 $locale{$lang}->{emails}" if (scalar(@emails) == 0);	
