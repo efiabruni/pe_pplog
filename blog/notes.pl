ca520fe5dea2c1657aaa24cae5cac10b33ebc6ab
efiabruni <svala.3@gmail.com>
2014-04-07 22:49:03 -0300

    added 'no entry' message to notes and a space to email to prevent error

--- blog/notes.pl
+++ blog/notes.pl
@@ -1,4 +1,3 @@
-
 my $page = r('page');
 my $cat = r('cat');
 my $do = '?do=notes';
@@ -6,51 +5,55 @@ my @notes=getFiles($config_DatabaseFolder.'/notes');
 my @categories;
 my @tempCategories = ();
 
-foreach(@notes)
+if (scalar(@notes) == 0){print $locale{$lang}->{nopages1}.' <a href="?do=newEntry">'.$locale{$lang}->{nopages2}.'</a>';}
+
+else
+{
+	foreach(@notes)
 		{
 			my @finalEntries = split(/¬/, $_);
 			my @split = split(/'/, $finalEntries[3]);
 			push(@tempCategories, @split);
 		}
-		@categories = array_unique(@tempCategories);
+	@categories = array_unique(@tempCategories);
 	
-print '<h1>'.$locale{$lang}->{categories}.'</h1>'; 
-foreach(@categories)
+	print '<h1>'.$locale{$lang}->{categories}.'</h1>'; 
+	foreach(@categories)
 	{
 		print '<a href="?do=notes&cat='.$_.'">'.$_.'</a><br />';
 	}
-unless ($cat eq ''){
-	my @thisCategoryEntries = ();
-	foreach my $item(@notes)
-	{
-		my @split = split(/¬/, $item);											# [0] = Title	[1] = Content	[2] = Date	[3] = Category
-		my @nextsplit = split(/'/,$split[3]);									# Efia change to accomodate more than one category
-		if (grep { $_ eq $cat } @nextsplit)
+	unless ($cat eq ''){
+		my @thisCategoryEntries = ();
+		foreach my $item(@notes)
 		{
-		push(@thisCategoryEntries, $item);
+			my @split = split(/¬/, $item);											# [0] = Title	[1] = Content	[2] = Date	[3] = Category
+			my @nextsplit = split(/'/,$split[3]);									# Efia change to accomodate more than one category
+			if (grep { $_ eq $cat } @nextsplit)
+			{
+				push(@thisCategoryEntries, $item);
+			}
 		}
+		@notes = @thisCategoryEntries;
+		$do = $do.'&cat='.$cat;
 	}
-	@notes = @thisCategoryEntries;
-	$do = $do.'&cat='.$cat;
-}
-# display notes
-if($page eq ''){ $page = 1; }	
-my $totalPages = ceil((scalar(@notes))/$config_entriesPerPage);								
-my $arrayEnd = ($config_entriesPerPage*$page);						# The array will start from this number
-my $arrayStart = $arrayEnd-($config_entriesPerPage-1);				# And loop till this number
-# As arrays start from 0, i will lower 1 to these values
-$arrayEnd--;
-$arrayStart--;
+	# display notes
+	if($page eq ''){ $page = 1; }	
+	my $totalPages = ceil((scalar(@notes))/$config_entriesPerPage);								
+	my $arrayEnd = ($config_entriesPerPage*$page);						# The array will start from this number
+	my $arrayStart = $arrayEnd-($config_entriesPerPage-1);				# And loop till this number
+	# As arrays start from 0, i will lower 1 to these values
+	$arrayEnd--;
+	$arrayStart--;
 
-my $i = $arrayStart;												# Start Looping...
-while($i<=$arrayEnd)
+	my $i = $arrayStart;												# Start Looping...
+	while($i<=$arrayEnd)
 		{
 			unless($notes[$i] eq '')
 			{
 				my @finalEntries = split(/¬/, $notes[$i]);
 				@categories = split (/'/, $finalEntries[3]);
-				print '<div class="article"><h1>'.$finalEntries[0].' &nbsp; <small><a href="?edit=notes/'.$finalEntries[4].'">'.$locale{$lang}->{e}.'</a> - <a href="?delete=notes/'.$finalEntries[4].'">'.$locale{$lang}->{d}.'</a></small></h1>
-				<form accept-charset="UTF-8" name="form1" method="post" style="display:inline; float:right;">
+				print '<div class="article"><h1>'.$finalEntries[0].' &nbsp; <small><a href="?edit=notes/'.$finalEntries[4].'">'.$locale{$lang}->{e}.'</a> - <a href="?delete=notes/'.$finalEntries[4].'">'.$locale{$lang}->{d}.'</a></small>
+				</h1><form accept-charset="UTF-8" name="form1" method="post" style="text-align:right;">
 				<input name="title" type="hidden" id="title" value="'.$finalEntries[0].'">
 				<input name="content" type="hidden" id="content" value="'.$finalEntries[1].'">
 				<input name="category" type="hidden" id="category" value="'.$finalEntries[3].'">
@@ -103,4 +106,5 @@ while($i<=$arrayEnd)
 			}
 		}
 	}
+}
 print '';
